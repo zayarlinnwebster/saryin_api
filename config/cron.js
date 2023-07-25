52cd34db3242d990fce39be1b479d44191f80725
zayarlinnwebster <zayarl513@gmail.com>
2023-07-25 21:32:31 +0630

    database backup with cron

---
+++ config/cron.js
@@ -0,0 +1,31 @@
+const local = require('./local');
+const fs = require('fs');
+const spawn = require('child_process').spawn;
+
+module.exports.cron = {
+  backupDB: {
+    schedule: '*/15 * * * *',
+    onTick: function () {
+      let wstream = fs.createWriteStream(local.BACKUP_PATH);
+      console.log(' cron: ', 'Running Database Backup Midnight');
+      let mysqldump = spawn('mysqldump', [
+        '-u',
+        local.MYSQL_USERNAME,
+        `-p${local.MYSQL_PASSWORD}`,
+        local.MYSQL_DBNAME,
+        `--no-create-info`
+      ]);
+      mysqldump
+        .stdout
+        .pipe(wstream)
+        .on('finish', () => {
+          console.log(' cron: ', 'Database Backup Completed');
+        })
+        .on('error', (err) => {
+          console.error(err);
+        });
+    },
+    runOnInit: true
+  },
+
+};
