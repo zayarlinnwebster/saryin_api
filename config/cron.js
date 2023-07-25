6436de1a49b9171410c9a43183a1f0670a8b405c
zayarlinnwebster <zayarl513@gmail.com>
2023-07-25 21:41:41 +0630

    update backup path

--- config/cron.js
+++ config/cron.js
@@ -6,7 +6,7 @@ module.exports.cron = {
   backupDB: {
     schedule: '*/15 * * * *',
     onTick: function () {
-      let wstream = fs.createWriteStream(local.BACKUP_PATH);
+      let wstream = fs.createWriteStream('../../backup/saryin_backup.sql');
       console.log(' cron: ', 'Running Database Backup Midnight');
       let mysqldump = spawn('mysqldump', [
         '-u',
@@ -15,15 +15,17 @@ module.exports.cron = {
         local.MYSQL_DBNAME,
         `--no-create-info`
       ]);
-      mysqldump
-        .stdout
-        .pipe(wstream)
-        .on('finish', () => {
-          console.log(' cron: ', 'Database Backup Completed');
-        })
-        .on('error', (err) => {
-          console.error(err);
-        });
+      setTimeout(() => {
+        mysqldump
+          .stdout
+          .pipe(wstream)
+          .on('finish', () => {
+            console.log(' cron: ', 'Database Backup Completed');
+          })
+          .on('error', (err) => {
+            console.error(err);
+          });
+      }, 6000 * 10);
     },
     runOnInit: true
   },
